2a431e41245583bc7c4f2ee707030695d947aaa8
Jma353 <jma353@cornell.edu>
2017-03-14 00:43:59 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+
+// TypeScript configuration 
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+  .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('default', ['watch']);
