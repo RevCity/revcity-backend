3b8dc0323c5befe6f05628b55a1f8225d83c3c5d
Jma353 <jma353@cornell.edu>
2017-03-24 16:35:58 -0400

    Add Google credential grabbing

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,16 +1,25 @@
 import {Router, Request, Response, NextFunction} from 'express';
 import * as util from 'util';
 import * as FB from 'fb';
+import * as Google from 'googleapis';
 
 class UsersRouter {
 
   router: Router;
 
+  /** Constructor **/
   constructor() {
     this.router = Router();
     this.init();
   }
 
+  /** Setup all endpoints **/
+  init() {
+    this.router.post('/fb', this.fbLogin);
+    this.router.post('/google', this.googleLogin);
+  }
+
+  /** Facebook Login **/
   public fbLogin(req: Request, res: Response, next: NextFunction) {
     const fields = ['id', 'name', 'email', 'picture.type(large)'];
     const token = req.param("accessToken");
@@ -19,16 +28,36 @@ class UsersRouter {
     });
   }
 
+  /** Google Login **/
   public googleLogin(req: Request, res: Response, next:  NextFunction) {
-    // TODO
-    res.json({});
-  }
 
-  init() {
-    this.router.post('/fb', this.fbLogin);
-    this.router.post('/google', this.googleLogin);
-  }
+    // OAuth2 client for the app
+    let oauth2Client = new Google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_SECRET_KEY,
+      process.env.GOOGLE_REDIRECT_URI
+    );
 
+    // Grab the id_token from the request
+    let token = req.param('idToken');
+
+    // Set the credential via the OAuth client
+    oauth2Client.setCredentials({
+      access_token: token
+    });
+
+    // Grab the person
+    Google.plus('v1').people.get({
+      userId: 'me',
+      auth: oauth2Client
+    }, (err, resp) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(resp);
+      }
+    });
+  }
 }
 
 const usersRouter = new UsersRouter();
